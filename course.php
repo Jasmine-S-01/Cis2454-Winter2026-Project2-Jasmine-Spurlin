694e7c083f7105d682f97601bad3aa172ec1a217
Jasmine <spurlin.jasmine@student.oaklandcc.edu>
2026-03-28 21:07:57 -0400

    Fixed Bug for Update Courses

--- course.php
+++ course.php
@@ -1,10 +1,42 @@
 <?php
 
-include 'models/database.php';
-include 'models/course.php';
+require_once 'models/database.php';
+require_once 'models/course.php';
+
+$action = htmlspecialchars(filter_input(INPUT_POST, "action"));
+
+$code = filter_input(INPUT_POST, "code");
+$name = htmlspecialchars(filter_input(INPUT_POST, "name"));
+$description = htmlspecialchars(filter_input(INPUT_POST, "description"));
+$credits = filter_input(INPUT_POST, "credits", FILTER_VALIDATE_FLOAT);
+
+
+if ($action == "insert_or_update" && $code != "" && $name != "" && $description != "" && $credits != 0) {
+    $insert_or_update = filter_input(INPUT_POST, 'insert_or_update');
+
+    $course = new Course($code, $name, $description, $credits);
+
+    if ($insert_or_update == "insert") {
+        insert_courses($course);
+    } else if ($insert_or_update == "update") {
+        update_stock($course);
+    }
+
+    header("Location: course.php");
+} else if ($action == "delete" && $code != "") {
+    // name and current price don't matter for delete
+   $course = new Course($code, "", "", 0);
+    delete_course($course);
+    header("Location: course.php");
+} else if ($action != "") {
+    $error_message = "Missing symbol, name, or current price";
+    include('views/error.php');
+}
+
 
 $courses = list_courses();
 
 include ('views/course.php');
 //CRUD Create Update Read Delete
 
+?>
